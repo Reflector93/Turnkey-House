6833f3392e550e0d4889199e4c47ba373da09ed9
cheverda.vsevolod <cheverda.vsevolod@gmail.com>
2022-05-13 19:28:09 +0300

    дальнейшие изменения

--- app/src/main/java/com/example/turnkeyhouse/MainActivity.java
+++ app/src/main/java/com/example/turnkeyhouse/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.turnkeyhouse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.graphics.BlendModeColorFilter;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
+
+import com.example.turnkeyhouse.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity
 {
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mainBinding.loginButton.getBackground().setColorFilter( new PorterDuffColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY) );
+        mainBinding.signUpButton.getBackground().setColorFilter( new PorterDuffColorFilter(Color.LTGRAY, PorterDuff.Mode.MULTIPLY) );
     }
 }
